a636e6c4edd27d2f307f8af9fb1199e3f93803bf
salaboy <salaboy@gmail.com>
2019-03-18 18:04:17 -0300

    change

--- src/main/java/com/salaboy/myservice1/MyService1Application.java
+++ src/main/java/com/salaboy/myservice1/MyService1Application.java
@@ -2,12 +2,20 @@ package com.salaboy.myservice1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MyService1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyService1Application.class, args);
+	}
+
+	@GetMapping
+	public String hello(){
+		return "Hi from services inside k8s!";
 	}
 
 }
